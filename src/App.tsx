bd277a3e31d6adc18a96d9bc7c27ef266f37ed2b
getchards <kazansasha02@gmail.com>
2024-08-20 12:08:29 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import './App.scss';
+import { TopBar } from './components/TopBar';
 
 export const App = () => (
   <div className="App">
-    <h1>Product Catalog</h1>
+    <TopBar />
   </div>
 );
